f8c111b6e71d609d08d650dd62536b1ed10dd95d
Philipp Siedler <Philipp.S@zaha-hadid.com>
2017-11-11 16:13:35 +0000

    changes Controls.cs

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Controls : MonoBehaviour {
 
     public float movementSpeed;
+    bool isMoving;
 
 	// Use this for initialization
 	void Start () {
@@ -14,22 +15,29 @@ public class Controls : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Vector2 movement = new Vector2();
+        isMoving = false;
         if (Input.GetKey(KeyCode.W))
         {
             movement += new Vector2(0.0f, movementSpeed);
+            isMoving = true;
         }
         if (Input.GetKey(KeyCode.A))
         {
             movement += new Vector2(-movementSpeed, 0.0f);
+            isMoving = true;
         }
         if (Input.GetKey(KeyCode.S))
         {
             movement += new Vector2(0.0f, -movementSpeed);
+            isMoving = true;
         }
         if (Input.GetKey(KeyCode.D))
         {
             movement += new Vector2(movementSpeed, 0.0f);
+            isMoving = true;
         }
         gameObject.transform.position += new Vector3(movement.x, movement.y, 0) * Time.deltaTime;
 	}
+
+    public bool getIsMoving() { return isMoving; }
 }
